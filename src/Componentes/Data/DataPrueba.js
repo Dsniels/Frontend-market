af6e7d76a7ef347d07e6559ed124ec30f054d0f8
Daniel Salazar <dsnile.salazr@gmail.com>
2023-07-21 01:14:37 -0600

    Data de prueba para producto

---
+++ src/Componentes/Data/DataPrueba.js
@@ -0,0 +1,32 @@
+export const productoArray = [
+    {
+        key : 1,
+        titulo : 'playera de tigres',
+        precio : 200
+    },
+    {
+        key : 2,
+        titulo : 'playera de tigres',
+        precio : 200
+    },
+    {
+        key : 3,
+        titulo : 'playera de tigres',
+        precio : 200
+    },
+    {
+        key : 4,
+        titulo : 'playera de tigres',
+        precio : 200
+    },
+    {
+        key : 5,
+        titulo : 'playera de tigres',
+        precio : 200
+    },
+    {
+        key : 6,
+        titulo : 'playera de tigres',
+        precio : 200
+    }
+]
