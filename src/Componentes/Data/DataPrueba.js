924c68ab2d80eee0d499feef5ae1bfe16017c1ff
Daniel Salazar <dsnile.salazr@gmail.com>
2023-12-15 19:25:00 -0600

    Data de prueba

--- src/Componentes/Data/DataPrueba.js
+++ src/Componentes/Data/DataPrueba.js
@@ -2,31 +2,37 @@ export const productoArray = [
     {
         key : 1,
         titulo : 'playera de tigres',
-        precio : 200
+        precio : 200,
+        marca: "nike"
     },
     {
         key : 2,
         titulo : 'playera de tigres',
-        precio : 200
+        precio : 200,
+        marca: "nike"
     },
     {
         key : 3,
         titulo : 'playera de tigres',
-        precio : 200
+        precio : 200,
+        marca: "nike"
     },
     {
         key : 4,
         titulo : 'playera de tigres',
-        precio : 200
+        precio : 200,
+        marca: "nike"
     },
     {
         key : 5,
         titulo : 'playera de tigres',
-        precio : 200
+        precio : 200,
+        marca: "nike"
     },
     {
         key : 6,
         titulo : 'playera de tigres',
-        precio : 200
+        precio : 200,
+        marca: "nike"
     }
 ]
