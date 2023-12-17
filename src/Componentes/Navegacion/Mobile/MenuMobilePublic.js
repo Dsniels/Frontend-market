f9f07b5436b0198f181d6831df44e30e3cb5c074
Daniel Salazar <dsnile.salazr@gmail.com>
2023-12-17 00:18:05 -0600

    Menu publico Mobile

---
+++ src/Componentes/Navegacion/Mobile/MenuMobilePublic.js
@@ -0,0 +1,32 @@
+import React from "react";
+import useStyles from "../../../Theme/useStyles";
+import { ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import { Link } from "react-router-dom/cjs/react-router-dom";
+
+const MenuMobilePublic = (props) => {
+
+    const classes = useStyles();
+    return(
+        <>
+            <ListItem button onClick={props.clickHandler} className={classes.listItem}>
+                <Link to="/login" className={classes.LinkBarmobile} >
+                    <ListItemIcon className={classes.listItemIcon}>
+                        <Icon>person</Icon>
+                    </ListItemIcon>
+                    <ListItemText>Login</ListItemText>
+                </Link>
+            </ListItem> 
+            <ListItem button onClick={props.clickHandler} className={classes.listItem}>
+                <Link to="/carrito" className={classes.LinkBarmobile} >
+                    <ListItemIcon className={classes.listItemIcon}>
+                        <Icon>shopping_cart</Icon>
+                    </ListItemIcon>
+                    <ListItemText>Mis pedidos</ListItemText>
+                </Link>
+            </ListItem>
+        
+        
+        </>
+    )
+}
+export default MenuMobilePublic;
