100e7ceb5a35ccff652befadbacd9f076c2a13b4
Daniel Salazar <dsnile.salazr@gmail.com>
2023-12-18 20:52:39 -0600

    Menu publico fixed

--- src/Componentes/Navegacion/Mobile/MenuMobilePublic.js
+++ src/Componentes/Navegacion/Mobile/MenuMobilePublic.js
@@ -1,6 +1,6 @@
 import React from "react";
 import useStyles from "../../../Theme/useStyles";
-import { ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import { Icon, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
 import { Link } from "react-router-dom/cjs/react-router-dom";
 
 const MenuMobilePublic = (props) => {
