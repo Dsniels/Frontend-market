47bd4af6e37060ca1bdecd4d8528ea212c3a29d6
Daniel Salazar <dsnile.salazr@gmail.com>
2024-03-15 18:52:55 -0600

    registro de reducer

--- src/contexto/reducers/index.js
+++ src/contexto/reducers/index.js
@@ -1,8 +1,10 @@
 import sesionUsuarioReducer from './sesionUsuarioReducer'
 import SesionCarritoReducer from './sesioncarritoReducer'
+import openSnackbarReducer from './openSnackbarReducer.js'
 
-export const mainReducer = ( {sesionUsuario, sesionCarrito}, action ) =>{
+export const mainReducer = ( {sesionUsuario, sesionCarrito, openSnackbar}, action ) =>{
     return{
+        openSnackbar : openSnackbarReducer(openSnackbar, action),
         sesionUsuario: sesionUsuarioReducer(sesionUsuario, action),
         sesionCarrito : SesionCarritoReducer(sesionCarrito, action)
     }
