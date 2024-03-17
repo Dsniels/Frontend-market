b557bd79e46e10f6e0dd6addb4332017950de0af
Daniel Salazar <dsnile.salazr@gmail.com>
2024-03-17 00:06:08 -0600

    Action para actualizar Usuario

--- src/actions/UsuarioAction.js
+++ src/actions/UsuarioAction.js
@@ -1,11 +1,34 @@
 import HttpCliente from '../Servicios/HttpCliente';
 import axios from 'axios';
-
+import {uploadImage} from '../Firebase/';
 
 const instancia = axios.create();
 instancia.CancelToken = axios.CancelToken;
 instancia.isCancel = axios.isCancel;
 
+export const actualizarUsuario = async (id, usuario, dispatch) => {
+    
+    if(usuario.file){
+        const urlImagen = await uploadImage(usuario.file);
+        usuario.imagen = urlImagen;
+
+    }
+
+    return new Promise ((resolve, eject) => {
+        HttpCliente.put("/api/usuario/actualizar/${id}", usuario).then(response => {
+            dispatch({
+                type: "ACTUALIZAR_USUARIO",
+                nuevoUsuario: response.data,
+                autenticado: true
+            });
+            resolve(response);
+        })
+        .catch(error => {
+                resolve(error.response);
+            })
+    });
+
+}
 
 
 export const registrarUsuario = (usuario, dispatch) =>{
